6b20b0ebf5819850a8422a209fd32e81d9bf70d8
Faruk Buğra DEMIREL <farukbugrademirel@gmail.com>
2020-11-02 17:39:33 +0100

    Complete both server and client concurrency func.

--- src/com/company/ChatClient.java
+++ src/com/company/ChatClient.java
@@ -2,13 +2,61 @@ package com.company;
 
 import java.io.*;
 import java.net.Socket;
+import java.util.concurrent.*;
+
 public class ChatClient {
 
     public static void main(String[] args) {
 
         try {
             Socket clientSocket = new Socket("localhost", 6666);
-            PrintStream out = new PrintStream(clientSocket.getOutputStream());
+
+            ExecutorService service = Executors.newFixedThreadPool(2);
+            service.submit(new ChatReceiver(clientSocket));
+            service.submit(new ChatSender(clientSocket));
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class ChatReceiver implements Runnable {
+
+    private final Socket socket;
+
+    public ChatReceiver(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            InputStreamReader reader = new InputStreamReader(socket.getInputStream());
+            BufferedReader inData = new BufferedReader(reader);
+            String text;
+            while((text = inData.readLine()) != null) {
+                System.out.println("Received:" + text);
+            }
+            socket.shutdownOutput();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+class ChatSender implements Runnable {
+
+    private final Socket socket;
+
+    public ChatSender(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try {
+            PrintStream out = new PrintStream(socket.getOutputStream());
             InputStreamReader reader = new InputStreamReader(System.in);
             BufferedReader inData = new BufferedReader(reader);
             String text;
@@ -17,7 +65,7 @@ public class ChatClient {
                 out.println(text);
                 System.out.println("Enter text to send: ");
             }
-            clientSocket.shutdownOutput();
+            socket.shutdownOutput();
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/com/company/MainChatServer.java
+++ src/com/company/MainChatServer.java
@@ -2,12 +2,14 @@ package com.company;
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class MainChatServer {
 
     private static final int port = 6666;
+    private final static ArrayList<Socket> clientList = new ArrayList<>();
 
     public static void main(String[] args) {
         ExecutorService executorService = null;
@@ -16,7 +18,8 @@ public class MainChatServer {
             ServerSocket ss = new ServerSocket(port);
             while(true) {
                 Socket s = ss.accept();
-                executorService.submit(new ChatServer(s));
+                executorService.submit(new ChatServer(s, clientList));
+                clientList.add(s);
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
@@ -33,19 +36,26 @@ public class MainChatServer {
 class ChatServer implements Runnable {
 
     private final Socket s;
+    private final ArrayList<Socket> clientList;
 
-    public ChatServer(Socket socket) {
+    public ChatServer(Socket socket, ArrayList<Socket> clientList) {
         this.s = socket;
+        this.clientList = clientList;
     }
 
     @Override
     public void run() {
         try {
             BufferedReader inData = new BufferedReader(new InputStreamReader(s.getInputStream()));
-            PrintStream out = new PrintStream(s.getOutputStream());
-            String text = "";
+            String text;
             while((text = inData.readLine()) != null) {
                 System.out.println("Received: " + text);
+                for (Socket s : clientList) {
+                    if(s != this.s) {
+                        PrintStream out = new PrintStream(s.getOutputStream());
+                        out.println(text);
+                    }
+                }
             }
         } catch (Exception e) {
             System.out.println(e.getMessage());
